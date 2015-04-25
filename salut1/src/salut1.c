c2f39051c20e2ac77f11d040dc61126889ae6531
mhcrnl <mhcrnl@gmail.com>
2015-04-25 16:42:23 +0300

    commit25/04/2015

---
+++ salut1/src/salut1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : salut1.c
+ Author      : mihai cornel
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
